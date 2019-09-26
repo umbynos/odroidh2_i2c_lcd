e53b17cad1e406c9593dcaed54ff720051c63400
umbynos <umberto.baldi@edu.unito.it>
2019-09-26 23:38:44 +0200

    make some optimization
    improvements in install.sh
    
    change sleep time

--- install.sh
+++ install.sh
@@ -1,64 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "# install dependent packages.."
-sleep 5
+sleep 1
 apt update && apt install i2c-tools git python3-dev libi2c-dev python3-smbus lm-sensors hdparm python3-psutil -y
 
 echo "# check i2c-dev module.."
-sleep 5
+sleep 1
 MODULE=`cat /etc/modules | grep i2c-dev`
 if [ "i2c-dev" != "$MODULE" ];then
   echo "i2c-dev" >> /etc/modules
   modprobe i2c-dev
   echo "# i2c-dev module not enabled.. enabling.."
   echo "# You need to restart ODROID-H2"
-  sleep 5
+  sleep 1
 fi
 
 echo "# download LCD python code..."
-sleep 5
+sleep 1
 mkdir -p /home/odroidlcd
-git clone https://github.com/djjproject/odroidh2_i2c_lcd /home/odroidlcd
+git clone https://github.com/umbynos/odroidh2_i2c_lcd /home/odroidlcd
 
 echo "# detect i2c ports.."
-sleep 5
-echo "# i2c port 1.."
-i2cdetect -y -r 1
-echo "# i2c port 2.."
-i2cdetect -y -r 2
-echo "# i2c port 3.."
-i2cdetect -y -r 3
-echo "# i2c port 4.."
-i2cdetect -y -r 4
-echo "# i2c port 5.."
-i2cdetect -y -r 5
-echo "# i2c port 6.."
-i2cdetect -y -r 6
-echo "# i2c port 7.."
-i2cdetect -y -r 7
-echo "# i2c port 8.."
-i2cdetect -y -r 8
-
-echo -e '# find address 27 and enter port number : \c'
-read PORT
-echo "# edit i2c port number.."
-sleep 5
-sed -i -e "s/BUS = 2/BUS = $PORT/g" /home/odroidlcd/lcddriver.py
+sleep 1
+for BUS in 1 2 3 4 5 6
+do
+	echo "# i2c port $BUS.."
+	if [[ $(i2cdetect -y -r $BUS | grep 27) ]]; then
+		echo "# i2c port found: $BUS"
+		echo "# edit i2c port number.."
+		sleep 1
+		sed -i -e "s/BUS = \w/BUS = $BUS/g" /home/odroidlcd/lcddriver.py
+	fi
+done
 
 echo "# install init.d script.."
-sleep 3
+sleep 1
 cp /home/odroidlcd/odroidlcd.sh /etc/init.d/odroidlcd
 chmod a+x /home/odroidlcd/run.sh
 chmod a+x /etc/init.d/odroidlcd
 update-rc.d odroidlcd defaults
 
 echo "# start oroidlcd service.."
-sleep 3
+sleep 1
 service odroidlcd start
 
 echo "# installation of odroidlcd service finished.."
-
-
-
-
-
