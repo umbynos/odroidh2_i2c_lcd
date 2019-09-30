fcbaee46df098c0513b688dfcd915d9692d2a85b
umbynos <umberto.baldi@edu.unito.it>
2019-09-30 23:31:12 +0200

    optimize shutdown script
    fix & optimize shutdown script

--- lcdoff.py
+++ lcdoff.py
@@ -1,19 +1,6 @@
-# import modules
 import lcddriver as lcd
-import os
-import psutil as ps
-from time import time
-from time import sleep
-from datetime import datetime
-
-lcd = lcd.lcd()
-
-lcd.display_string("####################",1)
-lcd.display_string("STOP ODROIDLCD...",2)
-lcd.display_string("SYSTEM SHUTDOWN...",3)
-lcd.display_string("####################",4)
-
-sleep(5)
-
 lcd.clear()
-lcd.backlight_off()
+lcd.display_string("     BYE! :)    ", 1)
+sleep(1)
+lcd.backlight(0)
+sys.exit()
