33235aa9a2bf0c1d0beb3a0179fe44350256d8e8
umbynos <umberto.baldi@edu.unito.it>
2019-09-30 23:55:26 +0200

    improve the port assignment which appears to be casual at startup

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+for BUS in 1 2 3 4 5 6
+do
+	if [[ $(i2cdetect -y -r $BUS | grep 27) ]]; then
+		sed -i -e "s/BUS = \w/BUS = $BUS/g" /home/odroidlcd/lcddriver.py
+	fi
+done
+
 /usr/bin/python3 /home/odroidlcd/sys_monitor_lcd.py
